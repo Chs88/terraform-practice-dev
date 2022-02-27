a4a1e69af5a46630afcfc12eb3656609c1661ce3
Chs88 <faskertikr@gmail.com>
2022-02-27 04:07:24 +0000

    Created route tables, routes and the associations

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -60,3 +60,99 @@ resource "aws_subnet" "public" {
     Role = "public"
     }
   }
+
+
+## Route tables and routes
+# I am going to define the routes separately from the route tables
+##And they will be seperately associated with the subnets
+
+# Private route table
+
+resource "aws_route_table" "private_rt" {
+
+  vpc_id = aws_vpc.main_vpc.id 
+
+  tags = {
+   
+    Name = "Project1-${var.infra_env}-private-rt"
+
+    Environment = var.infra_env
+
+    Project = "Project1"
+
+    ManagedBy = "Terraform"
+
+    Role = "private"
+
+    VPC = aws_vpc.main_vpc.id
+    }
+
+}
+
+# Public route table
+
+resource "aws_route_table" "public_rt" {
+
+  vpc_id = aws_vpc.main_vpc.id 
+
+  tags = {
+   
+    Name = "Project1-${var.infra_env}-public-rt"
+
+    Environment = var.infra_env
+
+    Project = "Project1"
+
+    ManagedBy = "Terraform"
+
+    Role = "public"
+
+    VPC = aws_vpc.main_vpc.id
+    }
+
+}
+
+##Private route
+
+resource "aws_route" "private" {
+
+  route_table_id = aws_route_table.private_rt.id
+
+  destination_cidr_block = "0.0.0.0/0" ##Any outgoing traffic
+
+  nat_gateway_id = aws_nat_gateway.nat_gw.id ##traffic goes through nat gw
+  
+}
+
+##Public route
+
+resource "aws_route" "public" {
+
+  route_table_id = aws_route_table.public_rt.id
+
+  destination_cidr_block = "0.0.0.0/0" ##Any outgoing traffic
+
+  gateway_id = aws_internet_gateway.internet_gw.id ##traffic goes through internet gateway
+  
+}
+
+##Route table associations
+
+resource "aws_route_table_association" "private" {
+
+  subnet_id      = aws_subnet.private.id
+
+  route_table_id = aws_route_table.private_rt.id
+
+}
+
+
+resource "aws_route_table_association" "public" {
+
+  subnet_id      = aws_subnet.public.id
+
+  route_table_id = aws_route_table.public_rt.id 
+  
+  }
+
+  
